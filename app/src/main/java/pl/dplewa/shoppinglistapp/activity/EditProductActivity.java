06809a4f7c60d38354b653d0e8b2292945513718
Dominik Plewa <plewad@gmail.com>
2018-11-04 11:15:25 +0100

    Add theme switching capability

--- app/src/main/java/pl/dplewa/shoppinglistapp/activity/EditProductActivity.java
+++ app/src/main/java/pl/dplewa/shoppinglistapp/activity/EditProductActivity.java
@@ -11,42 +11,31 @@ import pl.dplewa.shoppinglistapp.data.DatabaseOperations;
 /**
  * @author Dominik Plewa
  */
-public class EditProductActivity extends AppCompatActivity {
+public class EditProductActivity extends AbstractProductFormActivity {
 
     public static final String PRODUCT_NAME = "productName";
     public static final String PRODUCT_PRICE = "productPrice";
     public static final String PRODUCT_COUNT = "productCount";
     public static final String PRODUCT_ID = "rowid";
 
-    private DatabaseOperations dbOps;
-
-    private EditText nameField;
-    private EditText priceField;
-    private EditText countField;
     private int productId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_product_form);
-
         Bundle extras = getIntent().getExtras();
         productId = extras.getInt(PRODUCT_ID);
-        nameField = findViewById(R.id.productFormName);
         nameField.setText(extras.getString(PRODUCT_NAME));
-        priceField = findViewById(R.id.productFormPrice);
         priceField.setText(extras.getString(PRODUCT_PRICE));
-        countField = findViewById(R.id.productFormCount);
         countField.setText(extras.getString(PRODUCT_COUNT));
-        dbOps = new DatabaseOperations(this);
     }
 
-    public void addProduct(View view) {
+    @Override
+    protected void saveInternal(View view) {
         dbOps.updateProduct(productId,
                 nameField.getText().toString(),
                 priceField.getText().toString(),
                 Integer.parseInt(countField.getText().toString()));
-        finish();
     }
 }
--- app/src/main/java/pl/dplewa/shoppinglistapp/activity/SettingsActivity.java
+++ app/src/main/java/pl/dplewa/shoppinglistapp/activity/SettingsActivity.java
@@ -1,32 +1,51 @@
 package pl.dplewa.shoppinglistapp.activity;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import pl.dplewa.shoppinglistapp.R;
 
-public class SettingsActivity extends AppCompatActivity {
-    private static final String OPTION_1 = "option1";
-    private EditText editText;
+/**
+ * @author Dominik Plewa
+ */
+public class SettingsActivity extends ThemedActivity {
+    private String selectedTheme;
     private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
-        editText = findViewById(R.id.editText);
-        editText.setText(sharedPreferences.getString(OPTION_1, ""));
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        selectedTheme = sharedPreferences.getString(THEME_OPTION, THEME_LIGHT);
+        if (THEME_LIGHT.equals(selectedTheme)) {
+            ((RadioButton) findViewById(R.id.lightThemeRadioButton)).setChecked(true);
+        } else if (THEME_DARK.equals(selectedTheme)) {
+            ((RadioButton) findViewById(R.id.darkThemeRadioButton)).setChecked(true);
+        }
+        RadioGroup themeRadioGroup = findViewById(R.id.themeRadioGroup);
+        themeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.lightThemeRadioButton) {
+                    selectedTheme = THEME_LIGHT;
+                } else if (checkedId == R.id.darkThemeRadioButton) {
+                    selectedTheme = THEME_DARK;
+                }
+            }
+        });
     }
     public void saveSettings(View view) {
-        // TODO
-        String value = editText.getText().toString();
-        sharedPreferences.edit().putString(OPTION_1, value).apply();
-        Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
+        String originalTheme = sharedPreferences.getString(THEME_OPTION, THEME_LIGHT);
+        sharedPreferences.edit()
+                .putString(THEME_OPTION, selectedTheme)
+                .apply();
+        setResult(selectedTheme.equals(originalTheme) ? Activity.RESULT_CANCELED : Activity.RESULT_OK);
+        finish();
     }
 }
