ae0525cd363ce29921600a376a6c50008906e72c
Dominik Plewa <plewad@gmail.com>
2018-12-04 20:01:16 +0100

    Add basic user authentication

---
+++ shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/activity/AuthenticationActivity.java
@@ -0,0 +1,62 @@
+package pl.dplewa.shoppinglistapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+import java.util.List;
+
+import pl.dplewa.shoppinglistapp.R;
+
+/**
+ * @author Dominik Plewa
+ */
+public class AuthenticationActivity extends ThemedActivity {
+
+    private static final int RC_SIGN_IN = 44;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+
+    public void startSignIn(View view) {
+        final List<AuthUI.IdpConfig> authProviders = Collections.singletonList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(authProviders)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                Intent shoppingListIntent = new Intent(this, MainActivity.class);
+                shoppingListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(shoppingListIntent);
+            } else if (response != null) {
+                // an error occured
+                Toast.makeText(this, "Authentication failed, try again", Toast.LENGTH_LONG).show();
+            } else {
+                // TODO probably nothing? user pressed back button
+            }
+        }
+    }
+}
