fc5013ad09ad6e309c785355e3751973854b7869
Dominik Plewa <plewad@gmail.com>
2018-12-22 13:28:53 +0100

    force location updates for Geofence demo purposes

--- shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/ShoppingListApp.java
+++ shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/ShoppingListApp.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -19,6 +20,7 @@ import com.google.firebase.database.DatabaseError;
 
 import java.util.Collections;
 
+import pl.dplewa.shoppinglistapp.activity.shop.NoopIntentService;
 import pl.dplewa.shoppinglistapp.activity.shop.ShopTransitionsIntentService;
 import pl.dplewa.shoppinglistapp.data.Shop;
 import pl.dplewa.shoppinglistapp.data.ShopDatabase;
@@ -44,6 +46,12 @@ public class ShoppingListApp extends Application {
         this.mShopListener = new ShopToGeofenceListener();
         this.mGeofencePendingIntent = PendingIntent.getService(this, 0,
                 new Intent(this, ShopTransitionsIntentService.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        LocationRequest req = LocationRequest.create();
+        req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        req.setInterval(5000);
+        LocationServices.getFusedLocationProviderClient(this).requestLocationUpdates(req,
+                PendingIntent.getService(this, 0, new Intent(this, NoopIntentService.class), PendingIntent.FLAG_UPDATE_CURRENT));
 
         startService(new Intent(this, ShopTransitionsIntentService.class));
 
