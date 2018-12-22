1fc3938e2ccaa3bf07b57f9ea4b2a38c341e04c8
Dominik Plewa <plewad@gmail.com>
2018-12-22 22:12:41 +0100

    move initialization of user data related services to after login

--- shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/ShoppingListApp.java
+++ shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/ShoppingListApp.java
@@ -30,23 +30,9 @@ import pl.dplewa.shoppinglistapp.data.ShopDatabase;
  */
 public class ShoppingListApp extends Application {
 
-    private static final String TAG = ShoppingListApp.class.getSimpleName();
-
-    private ShopDatabase mShopDb;
-    private GeofencingClient mGeofencingClient;
-    private PendingIntent mGeofencePendingIntent;
-    private ChildEventListener mShopListener;
-
     @Override
     public void onCreate() {
         super.onCreate();
-
-        this.mShopDb = new ShopDatabase();
-        this.mGeofencingClient = LocationServices.getGeofencingClient(this);
-        this.mShopListener = new ShopToGeofenceListener();
-        this.mGeofencePendingIntent = PendingIntent.getService(this, 0,
-                new Intent(this, ShopTransitionsIntentService.class), PendingIntent.FLAG_UPDATE_CURRENT);
-
         LocationRequest req = LocationRequest.create();
         req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         req.setInterval(5000);
@@ -54,72 +40,5 @@ public class ShoppingListApp extends Application {
                 PendingIntent.getService(this, 0, new Intent(this, NoopIntentService.class), PendingIntent.FLAG_UPDATE_CURRENT));
 
         startService(new Intent(this, ShopTransitionsIntentService.class));
-
-        mShopDb.getDb().addChildEventListener(mShopListener);
-    }
-
-    private class ShopToGeofenceListener implements ChildEventListener {
-
-        @Override
-        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            final String id = dataSnapshot.getKey();
-            final Shop shop = dataSnapshot.getValue(Shop.class);
-            addGeofence(id, shop);
-        }
-
-        @Override
-        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            final String id = dataSnapshot.getKey();
-            final Shop shop = dataSnapshot.getValue(Shop.class);
-            mGeofencingClient.removeGeofences(Collections.singletonList(id))
-                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            addGeofence(id, shop);
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e(TAG, "Failed to remove geofence", e);
-                        }
-                    });
-        }
-
-        @Override
-        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-            final String id = dataSnapshot.getKey();
-            mGeofencingClient.removeGeofences(Collections.singletonList(id));
-        }
-
-        @Override
-        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-            // do nothing
-        }
-
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError) {
-            Log.e(TAG, "Shop event listener cancelled", databaseError.toException());
-        }
-
-
-        private void addGeofence(String id, Shop shop) {
-            mGeofencingClient.addGeofences(
-                    new GeofencingRequest.Builder()
-                            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
-                            .addGeofence(new Geofence.Builder()
-                                    .setRequestId(id)
-                                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
-                                    .setCircularRegion(shop.latitude, shop.longtitude, shop.radius.floatValue())
-                                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
-                                    .build()).build(),
-                    mGeofencePendingIntent)
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.e(TAG, "Failed to add geofence", e);
-                        }
-                    });
-        }
     }
 }
--- shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/activity/AuthenticationActivity.java
+++ shopping-list/app/src/main/java/pl/dplewa/shoppinglistapp/activity/AuthenticationActivity.java
@@ -1,18 +1,35 @@
 package pl.dplewa.shoppinglistapp.activity;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 import java.util.Collections;
 import java.util.List;
 
 import pl.dplewa.shoppinglistapp.R;
 import pl.dplewa.shoppinglistapp.activity.product.ProductListActivity;
+import pl.dplewa.shoppinglistapp.activity.shop.ShopTransitionsIntentService;
+import pl.dplewa.shoppinglistapp.data.Shop;
+import pl.dplewa.shoppinglistapp.data.ShopDatabase;
 
 /**
  * @author Dominik Plewa
@@ -46,6 +63,8 @@ public class AuthenticationActivity extends ThemedActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
+                new ShopDatabase().getDb()
+                        .addChildEventListener(new ShopToGeofenceListener(this));
                 // Successfully signed in
                 Intent shoppingListIntent = new Intent(this, ProductListActivity.class);
                 shoppingListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -56,6 +75,83 @@ public class AuthenticationActivity extends ThemedActivity {
             } else {
                 // TODO probably nothing? user pressed back button
             }
+        }
+    }
+
+    private static final class ShopToGeofenceListener implements ChildEventListener {
+
+        private static final String TAG = ShopToGeofenceListener.class.getSimpleName();
+
+        private GeofencingClient mGeofencingClient;
+        private PendingIntent mGeofencePendingIntent;
+
+        private ShopToGeofenceListener(Context context) {
+            this.mGeofencingClient = LocationServices.getGeofencingClient(context);
+            this.mGeofencePendingIntent = PendingIntent.getService(context, 0,
+                    new Intent(context, ShopTransitionsIntentService.class),
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+        }
+
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            final String id = dataSnapshot.getKey();
+            final Shop shop = dataSnapshot.getValue(Shop.class);
+            addGeofence(id, shop);
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            final String id = dataSnapshot.getKey();
+            final Shop shop = dataSnapshot.getValue(Shop.class);
+            mGeofencingClient.removeGeofences(Collections.singletonList(id))
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            addGeofence(id, shop);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e(TAG, "Failed to remove geofence", e);
+                        }
+                    });
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            final String id = dataSnapshot.getKey();
+            mGeofencingClient.removeGeofences(Collections.singletonList(id));
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            // do nothing
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.e(TAG, "Shop event listener cancelled", databaseError.toException());
+        }
+
+
+        private void addGeofence(String id, Shop shop) {
+            mGeofencingClient.addGeofences(
+                    new GeofencingRequest.Builder()
+                            .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                            .addGeofence(new Geofence.Builder()
+                                    .setRequestId(id)
+                                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                                    .setCircularRegion(shop.latitude, shop.longtitude, shop.radius.floatValue())
+                                    .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                                    .build()).build(),
+                    mGeofencePendingIntent)
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e(TAG, "Failed to add geofence", e);
+                        }
+                    });
         }
     }
 }
